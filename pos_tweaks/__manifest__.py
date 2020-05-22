31f4724c094d4e2f634bab9fff56bca8d85898d7
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2020-05-22 12:54:00 +0200

    Issue24971: Add Total column in Sale Detail report

---
+++ pos_tweaks/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2020 Stein & Gabelgaard ApS
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Pos Tweaks',
+    'description': """
+        Misc. POS UI Tweaks""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Stein & Gabelgaard ApS',
+    'website': 'www.steingabelgaard.dk',
+    'depends': [
+        'point_of_sale',
+    ],
+    'data': [
+        'views/report_saledetails.xml',
+    ],
+    'demo': [
+    ],
+}
